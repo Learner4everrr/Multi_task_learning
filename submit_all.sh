ef1fb5a3b1a4f2f472a572fc56ecab9bf9493373
Learner4everrr <zhan8023@umn.edu>
2024-06-11 22:43:28 -0500

    update

---
+++ submit_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 检查是否提供了任务数量参数
+if [ -z "$1" ]; then
+  echo "Usage: $0 <number_of_tasks>"
+  exit 1
+fi
+
+# 提交指定数量的 submit.sh 文件
+for i in $(seq 1 $1); do
+    sbatch submit_$i.sh
+done
