a9832bfd4f84e8f4c863f9f3b018ded56996663c
Learner4everrr <zhan8023@umn.edu>
2024-06-15 00:17:45 -0500

    update

--- 0_train.py
+++ 0_train.py
@@ -80,9 +80,10 @@ def formatting_func(example):
 def prepare_data(args):
     files = args.sourcefiles.split(',')
     # print(files)
-    
+    print('3'*1000)
     # Load datasets
     datasets = [load_dataset('json', data_files='datasets/'+file+'.json')['train'] for file in files]
+    print('4'*1000)
     if len(datasets) == 1:
         trainingset = datasets[0]
     else:
@@ -197,10 +198,10 @@ def train_model(args, trainingset):
 
 def main():
     args = read_argument()
-    # print('1'*1000)
+    print('1'*1000)
     # print(args)
     trainingset = prepare_data(args)
-    # print('2'*1000)
+    print('2'*1000)
     print(len(trainingset))
     train_model(args, trainingset)
 
